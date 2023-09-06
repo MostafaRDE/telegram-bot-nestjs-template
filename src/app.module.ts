c38e77099af7461e02ae15e1c50a00664145787f
Mostafa Effati <mostafa.rostamdokht@gmail.com>
2023-09-06 10:53:47 +0330

    chore: Adding app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common'
+import { externalsImportsAppModule } from './app/imports/externals.import'
+import { internalsImportsAppModule } from './app/imports/internals.import'
 
-@Module({})
+@Module({
+    imports: [
+        ...externalsImportsAppModule,
+        ...internalsImportsAppModule,
+    ],
+})
 export class AppModule {}
---
+++ src/app/imports/internals.import.ts
@@ -0,0 +1,5 @@
+import { BotModule } from '../../modules/bot/bot.module'
+
+export const internalsImportsAppModule = [
+    BotModule,
+]
