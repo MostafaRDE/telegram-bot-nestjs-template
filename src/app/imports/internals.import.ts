6597379713e815679726924455def921b1bb9f08
Mostafa Effati <mostafa.rostamdokht@gmail.com>
2023-09-15 11:20:11 +0330

    chore: Creating job-manager module for managing queue of users messages

--- src/app/imports/internals.import.ts
+++ src/app/imports/internals.import.ts
@@ -1,5 +1,7 @@
 import { BotModule } from '../../modules/bot/bot.module'
+import { JobManager } from '../../modules/job-manager/job-manager.module'
 
 export const internalsImportsAppModule = [
     BotModule,
+    JobManager,
 ]
