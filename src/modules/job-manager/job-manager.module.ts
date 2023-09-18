f57041249c11610bfd607dff740740661fbad724
Mostafa Effati <mostafa.rostamdokht@gmail.com>
2023-09-18 22:08:44 +0330

    chore: Registering entry queue in job manager module

--- src/modules/job-manager/job-manager.module.ts
+++ src/modules/job-manager/job-manager.module.ts
@@ -1,8 +1,13 @@
 import { Global, Module } from '@nestjs/common'
 import { JobManagerService } from './job-manager.service'
+import { BullModule } from '@nestjs/bull'
+import { QueuesEnum } from './enums/queues.enum'
 
 @Global()
 @Module({
+    imports: [
+        BullModule.registerQueue({ name: QueuesEnum.Entry }),
+    ],
     providers: [
         JobManagerService,
     ],
