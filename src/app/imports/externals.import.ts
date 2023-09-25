5e9a51258fe572e6f56a1569c30e83d3dd1b5641
Mostafa Effati <mostafa.rostamdokht@gmail.com>
2023-09-25 22:53:59 +0330

    chore: Adding mongodb with mongoose

--- src/app/imports/externals.import.ts
+++ src/app/imports/externals.import.ts
@@ -1,7 +1,9 @@
+import { BullModule } from '@nestjs/bull'
 import { CacheModule } from '@nestjs/cache-manager'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 import { configLoader } from '../../config'
-import { BullModule } from '@nestjs/bull'
+import { MongooseConfigService } from './plugins/mongoose.config.service'
 
 export const externalsImportsAppModule = [
     CacheModule.register({ isGlobal: true }),
@@ -13,7 +15,7 @@ export const externalsImportsAppModule = [
             redis: configService.get('database.redis'),
         }),
     }),
-    // MongooseModule.forRootAsync({
-    //     useClass: MongooseConfigService,
-    // }),
+    MongooseModule.forRootAsync({
+        useClass: MongooseConfigService,
+    }),
 ]
